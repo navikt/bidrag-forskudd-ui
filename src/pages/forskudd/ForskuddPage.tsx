41d261061b275e2ccd27dc92a0c7cd6028747b0a
andrii <25188+unorsk@users.noreply.github.com>
2023-02-08 13:21:18 +0100

    legger til en kommando for å generere  en axios klienteog et eksempel på hvordan den kan brukes

--- src/pages/forskudd/ForskuddPage.tsx
+++ src/pages/forskudd/ForskuddPage.tsx
@@ -1,18 +1,23 @@
+import { useApi } from "@navikt/bidrag-ui-common";
 import { Button } from "@navikt/ds-react";
 import React, { useEffect, useState } from "react";
-
-import PersonService from "../../service/PersonService";
+import { Api as PersonApi } from "../../api/PersonApi";
 import PageWrapper from "../PageWrapper";
 
 interface ForskuddPageProps {
     personId: string;
 }
+
 export default function ForskuddPage({ personId }: ForskuddPageProps) {
     const [personNavn, setPersonNavn] = useState<string>();
+    const personApi = useApi(new PersonApi({baseURL: process.env.BIDRAG_PERSON_URL}), "bidrag-person", "fss");
 
     useEffect(() => {
-        new PersonService().hentPerson(personId).then((res) => setPersonNavn(res.navn));
-    }, []);
+        personApi.informasjon.hentPerson(personId)
+        .then((res) => {
+            setPersonNavn(res.data.navn);
+        })
+    }, [])
 
     return (
         <PageWrapper name={"Forskudd"}>
