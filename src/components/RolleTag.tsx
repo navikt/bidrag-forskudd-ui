ff9b0055ede0a169ba25cfb066f11420b2f91a9d
andrii <25188+unorsk@users.noreply.github.com>
2023-02-13 12:29:14 +0100

    lint

--- src/components/RolleTag.tsx
+++ src/components/RolleTag.tsx
@@ -1,10 +1,13 @@
-import { Tag } from "@navikt/ds-react"
-import { ROLE_TAGS } from "../constants/roleTags"
-import { RolleType } from "../enum/RolleType";
+import { Tag } from "@navikt/ds-react";
 import React from "react";
 
-export const RolleTag = ({rolleType}: {rolleType: RolleType}) => {
-  return <Tag variant={ROLE_TAGS[rolleType]} size="small" className="w-8 mr-2 rounded">
-          {rolleType}
-      </Tag>
-}
+import { ROLE_TAGS } from "../constants/roleTags";
+import { RolleType } from "../enum/RolleType";
+
+export const RolleTag = ({ rolleType }: { rolleType: RolleType }) => {
+    return (
+        <Tag variant={ROLE_TAGS[rolleType]} size="small" className="w-8 mr-2 rounded">
+            {rolleType}
+        </Tag>
+    );
+};
--- src/components/forms/Vedtak.tsx
+++ src/components/forms/Vedtak.tsx
@@ -1,8 +1,8 @@
 import { ExternalLink } from "@navikt/ds-icons";
-import { Button, ConfirmationPanel, Heading, Link, Table, Tag } from "@navikt/ds-react";
+import { Button, ConfirmationPanel, Heading, Link, Table } from "@navikt/ds-react";
 import React from "react";
-import { RolleType } from "../../enum/RolleType";
 
+import { RolleType } from "../../enum/RolleType";
 import { CommonFormProps } from "../../pages/forskudd/ForskuddPage";
 import { RolleDetaljer } from "../RolleDetaljer";
 import { RolleTag } from "../RolleTag";
@@ -31,52 +31,48 @@ export default function Vedtak({ setActiveStep }: CommonFormProps) {
             resultat: "Opph pga høy inntekt",
             beløp: 0,
         },
-    ]
+    ];
     return (
         <div>
             <Heading level="2" size="large">
                 Fatte vedtak
             </Heading>
-            <div>
-                Totrinnskontroll: inntekt
-            </div>
+            <div>Totrinnskontroll: inntekt</div>
             <Heading level="3" size="medium">
                 Oppsummering
             </Heading>
-            <div>
-            Barn i egen husstand: 
-            </div>
+            <div>Barn i egen husstand:</div>
             <Table>
                 <Table.Header>
                     <Table.Row>
-                    <Table.HeaderCell scope="col">Rolle</Table.HeaderCell>
-                    <Table.HeaderCell scope="col">Fødselsnummer</Table.HeaderCell>
-                    <Table.HeaderCell scope="col">Navn</Table.HeaderCell>
-                    <Table.HeaderCell scope="col">Type søknad</Table.HeaderCell>
-                    <Table.HeaderCell scope="col">Periode</Table.HeaderCell>
-                    <Table.HeaderCell scope="col">Inntekt</Table.HeaderCell>
-                    <Table.HeaderCell scope="col">Sivilstand til BM</Table.HeaderCell>
-                    <Table.HeaderCell scope="col">Resultat</Table.HeaderCell>
-                    <Table.HeaderCell scope="col">Beløp</Table.HeaderCell>
+                        <Table.HeaderCell scope="col">Rolle</Table.HeaderCell>
+                        <Table.HeaderCell scope="col">Fødselsnummer</Table.HeaderCell>
+                        <Table.HeaderCell scope="col">Navn</Table.HeaderCell>
+                        <Table.HeaderCell scope="col">Type søknad</Table.HeaderCell>
+                        <Table.HeaderCell scope="col">Periode</Table.HeaderCell>
+                        <Table.HeaderCell scope="col">Inntekt</Table.HeaderCell>
+                        <Table.HeaderCell scope="col">Sivilstand til BM</Table.HeaderCell>
+                        <Table.HeaderCell scope="col">Resultat</Table.HeaderCell>
+                        <Table.HeaderCell scope="col">Beløp</Table.HeaderCell>
                     </Table.Row>
                 </Table.Header>
                 <Table.Body>
                     {data.map((item, i) => {
-                    return (
-                        <Table.Row key={i + item.fnmr}>
-                            <Table.DataCell>
-                                <RolleTag rolleType={item.rolle} />
-                            </Table.DataCell>
-                            <Table.DataCell>{item.fnmr}</Table.DataCell>
-                            <Table.DataCell>{item.navn}</Table.DataCell>
-                            <Table.DataCell>{item.type}</Table.DataCell>
-                            <Table.DataCell>{item.periode}</Table.DataCell>
-                            <Table.DataCell>{item.inntekt}</Table.DataCell>
-                            <Table.DataCell>{item.sivilsStandBm}</Table.DataCell>
-                            <Table.DataCell>{item.resultat}</Table.DataCell>
-                            <Table.DataCell>{item.beløp}</Table.DataCell>
-                        </Table.Row>
-                    );
+                        return (
+                            <Table.Row key={i + item.fnmr}>
+                                <Table.DataCell>
+                                    <RolleTag rolleType={item.rolle} />
+                                </Table.DataCell>
+                                <Table.DataCell>{item.fnmr}</Table.DataCell>
+                                <Table.DataCell>{item.navn}</Table.DataCell>
+                                <Table.DataCell>{item.type}</Table.DataCell>
+                                <Table.DataCell>{item.periode}</Table.DataCell>
+                                <Table.DataCell>{item.inntekt}</Table.DataCell>
+                                <Table.DataCell>{item.sivilsStandBm}</Table.DataCell>
+                                <Table.DataCell>{item.resultat}</Table.DataCell>
+                                <Table.DataCell>{item.beløp}</Table.DataCell>
+                            </Table.Row>
+                        );
                     })}
                 </Table.Body>
             </Table>
@@ -85,18 +81,21 @@ export default function Vedtak({ setActiveStep }: CommonFormProps) {
                 Forsendelse gjelder:
             </Heading>
             <div>
-                <RolleDetaljer rolle={{
-                    navn: "Mia  Cathrine Svendsen",
-                    fulltNavn: "Mia  Cathrine Svendsen",
-                    fodselsnummer: "081020 34566",
-                    type: RolleType.BM
-                }} />
+                <RolleDetaljer
+                    rolle={{
+                        navn: "Mia  Cathrine Svendsen",
+                        fulltNavn: "Mia  Cathrine Svendsen",
+                        fodselsnummer: "081020 34566",
+                        type: RolleType.BM,
+                    }}
+                />
             </div>
             <Heading level="3" size="medium">
                 Sjekk notat
             </Heading>
             <div>
-            Så snart vedtaket er fattet, kan den gjenfinnes i sakshistorik. Notatet blir generert automatisk basert  på opplysningene oppgitt.   
+                Så snart vedtaket er fattet, kan den gjenfinnes i sakshistorik. Notatet blir generert automatisk basert
+                på opplysningene oppgitt.
             </div>
             <div>
                 <Link href="#" onClick={() => {}} className="font-bold">
@@ -108,7 +107,7 @@ export default function Vedtak({ setActiveStep }: CommonFormProps) {
                     // checked={state}
                     label="Jeg har sjekket notat og opplysninger i søknaden og bekrefter at opplysningene stemmer."
                     onChange={() => {}}
-                    >
+                >
                     bekreft tekst
                 </ConfirmationPanel>
             </div>
