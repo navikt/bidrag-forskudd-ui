4fbe6d467f83231943639eb23ebaadd4e3c18739
andrii <25188+unorsk@users.noreply.github.com>
2023-02-13 09:39:43 +0100

    start på vedtak

--- src/components/forms/Vedtak.tsx
+++ src/components/forms/Vedtak.tsx
@@ -1,4 +1,5 @@
-import { Heading } from "@navikt/ds-react";
+import { ExternalLink } from "@navikt/ds-icons";
+import { Button, Heading, Link } from "@navikt/ds-react";
 import React from "react";
 
 import { CommonFormProps } from "../../pages/forskudd/ForskuddPage";
@@ -7,8 +8,50 @@ export default function Vedtak({ setActiveStep }: CommonFormProps) {
     return (
         <div>
             <Heading level="2" size="large">
-                Vedtak
+                Fatte vedtak
             </Heading>
+            <div>
+                Totrinnskontroll: inntekt
+            </div>
+            <Heading level="3" size="medium">
+                Oppsummering
+            </Heading>
+            <div>
+            Barn i egen husstand: 
+            </div>
+            <div>
+                tabel
+            </div>
+
+            <Heading level="3" size="medium">
+                Forsendelse gjelder:
+            </Heading>
+            <div>
+                Mia  Cathrine Svendsen 
+            </div>
+            <Heading level="3" size="medium">
+                Sjekk notat
+            </Heading>
+            <div>
+            Så snart vedtaket er fattet, kan den gjenfinnes i sakshistorik. Notatet blir generert automatisk basert  på opplysningene oppgitt.   
+            </div>
+            <div>
+                Sjekk notat
+            </div>
+            <div>
+                Bekreft
+            </div>
+            <div className="mt-4">
+                
+            <div>
+                    <Button loading={false} onClick={() => {}} className="w-max">
+                        Fatte veddtak
+                    </Button>
+                    <Button loading={false} variant="secondary" onClick={() => {}} className="w-max">
+                        Avbryt
+                    </Button>
+                </div>
+            </div>
         </div>
     );
 }
