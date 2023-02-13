9a6ac088a8b2beaed5823954fde0546e65840094
markodraksic <draksic.m@gmail.com>
2023-02-13 16:04:25 +0100

    Align items vertically

--- src/components/RolleDetaljer.tsx
+++ src/components/RolleDetaljer.tsx
@@ -6,7 +6,11 @@ import { RolleTag } from "./RolleTag";
 
 export const RolleDetaljer = ({ rolle, withBorder = true }: { rolle: IRollerUi; withBorder?: boolean }) => {
     return (
-        <div className={`px-6 py-2 ${withBorder && "border-[var(--a-border-divider)] border-solid border-b"} flex`}>
+        <div
+            className={`px-6 py-2 ${
+                withBorder && "border-[var(--a-border-divider)] border-solid border-b"
+            } flex items-center`}
+        >
             <RolleTag rolleType={rolle.type} />
             <span className="w-64">{rolle.fulltNavn}</span>
             <span className="mx-4">/</span> {rolle.fodselsnummer}
