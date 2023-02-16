0c731f7d77b2fe9c502c285e5f2a1297007c515d
andrii <25188+unorsk@users.noreply.github.com>
2023-02-16 14:35:48 +0100

    legger til bisys url og vedtak api

--- src/components/forms/Vedtak.tsx
+++ src/components/forms/Vedtak.tsx
@@ -2,8 +2,10 @@ import { ExternalLink } from "@navikt/ds-icons";
 import { SuccessStroke } from "@navikt/ds-icons";
 import { Button, ConfirmationPanel, Heading, Label, Link, Table } from "@navikt/ds-react";
 import React, { useState } from "react";
+import { useForskudd } from "../../context/ForskuddContext";
 
 import { RolleType } from "../../enum/RolleType";
+import environment from "../../environment";
 import { CommonFormProps } from "../../pages/forskudd/ForskuddPage";
 import { FlexRow } from "../layout/grid/FlexRow";
 import { RolleDetaljer } from "../RolleDetaljer";
@@ -11,7 +13,8 @@ import { RolleTag } from "../RolleTag";
 
 export const Vedtak = ({ setActiveStep }: CommonFormProps) => {
     const [erBekreftet, setBekreftet] = useState(false);
-
+    const { sak } = useForskudd();
+    
     const rolle = {
         navn: "Mia  Cathrine Svendsen",
         fulltNavn: "Mia  Cathrine Svendsen",
@@ -147,7 +150,11 @@ export const Vedtak = ({ setActiveStep }: CommonFormProps) => {
                 <Button disabled={!erBekreftet} loading={false} onClick={sendeVedtak} className="w-max" size="small">
                     Fatte vedtak
                 </Button>
-                <Button loading={false} variant="secondary" onClick={() => {}} className="w-max" size="small">
+                <Button loading={false} variant="secondary" onClick={() => {
+                    // TODO: legge til en sjekk/bekreftelse for å gå tilbake til bisys
+                    // og kanskje stateId?
+                    window.location.href = `${environment.url.bisys}Oppgaveliste.do`;
+                }} className="w-max" size="small">
                     Avbryt
                 </Button>
             </FlexRow>
