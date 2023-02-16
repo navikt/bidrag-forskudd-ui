74dc1596c26dea802dda32130f8d32ab8c7816bb
markodraksic <draksic.m@gmail.com>
2023-02-16 09:41:05 +0100

    Generate random names for mock api

--- src/__mocks__/msw/personMock.ts
+++ src/__mocks__/msw/personMock.ts
@@ -14,10 +14,71 @@ export default function personMock(): RestHandler[] {
                 ctx.body(
                     JSON.stringify({
                         ident: requestBody.ident,
-                        navn: "Etternavn, Fornavn Middelnavn",
+                        navn: generateName(),
                     } as PersonDto)
                 )
             );
         }),
     ];
 }
+
+const names = {
+    female: [
+        "Nora",
+        "Emma",
+        "Olivia",
+        "Ella",
+        "Sofie",
+        "Sophie",
+        "Leah",
+        "Lea",
+        "Frida",
+        "Iben",
+        "Sofia",
+        "Sophia",
+        "Sarah",
+        "Zara",
+    ],
+    male: [
+        "Jakob",
+        "Jacob",
+        "Noah",
+        "Noa",
+        "Emil",
+        "Lucas",
+        "Lukas",
+        "Oliver",
+        "Isak",
+        "William",
+        "Filip",
+        "Philip",
+        "Aksel",
+        "Theodor",
+    ],
+    lastName: [
+        "Hansen",
+        "Johansen",
+        "Olsen",
+        "Larsen",
+        "Andersen",
+        "Pedersen",
+        "Nilsen",
+        "Kristiansen",
+        "Jensen",
+        "Karlsen",
+        "Johnsen",
+        "Pettersen",
+        "Eriksen",
+        "Berg",
+        "Haugen",
+    ],
+};
+
+const generateName = () => {
+    const sex = Math.random() < 0.5 ? "female" : "male";
+    const firstName = names[sex][Math.floor(Math.random() * names[sex].length)];
+    const middleName = names[sex][Math.floor(Math.random() * names[sex].length)];
+    const lastName = names["lastName"][Math.floor(Math.random() * names["lastName"].length)];
+
+    return `${lastName}, ${firstName} ${middleName}`;
+};
