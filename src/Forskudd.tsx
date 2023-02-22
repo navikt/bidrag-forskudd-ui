b665142595b0750613f71707bd13070d672a24e7
markodraksic <draksic.m@gmail.com>
2023-02-22 10:12:00 +0100

    Move initialization of mock to Forskudd until backend is ready

--- src/Forskudd.tsx
+++ src/Forskudd.tsx
@@ -1,11 +1,14 @@
 import { QueryClient, QueryClientProvider } from "react-query";
 import { BrowserRouter, Route, Routes, useParams } from "react-router-dom";
 
+import { initMock } from "./__mocks__/msw";
 import { ForskuddProvider } from "./context/ForskuddContext";
 import { ForskuddPage } from "./pages/forskudd/ForskuddPage";
 
 const queryClient = new QueryClient();
 
+// TODO: move initMock() to app.tsx once backend is ready
+initMock();
 export default function Forskudd() {
     return (
         <BrowserRouter>
