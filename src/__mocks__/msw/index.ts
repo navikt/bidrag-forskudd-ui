e3682f2b9410aca87a4c8e584e15c4b8ca15a281
Marko <draksic.m@gmail.com>
2023-09-05 10:33:46 +0200

    Return back check for mock env (#35)

--- src/__mocks__/msw/index.ts
+++ src/__mocks__/msw/index.ts
@@ -1,18 +1,18 @@
 export function initMock() {
-    // TODO: check if we should run mock once we have backend ready, until then run mock in all environments
-    // process.env.NODE_ENV === "development" && process.env.ENABLE_MOCK == "true"
-    const { worker } = require("./browser");
-    worker
-        .start({
-            onUnhandledRequest: "warn",
-            waitUntilReady: true,
-            serviceWorker: {
-                url: `/mockServiceWorker.js`,
-                options: {
-                    scope: "/",
+    if (process.env.NODE_ENV === "development" && process.env.ENABLE_MOCK == "true") {
+        const { worker } = require("./browser");
+        worker
+            .start({
+                onUnhandledRequest: "warn",
+                waitUntilReady: true,
+                serviceWorker: {
+                    url: `/mockServiceWorker.js`,
+                    options: {
+                        scope: "/",
+                    },
                 },
-            },
-        })
-        .then(console.log)
-        .catch((e) => console.log(e));
+            })
+            .then(console.log)
+            .catch((e) => console.log(e));
+    }
 }
