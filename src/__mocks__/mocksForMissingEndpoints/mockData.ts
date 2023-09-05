dfa782f681234555942c1441e43277269568472b
markodraksic <draksic.m@gmail.com>
2023-09-05 10:28:55 +0200

    Return back check for mock env

--- src/__mocks__/mocksForMissingEndpoints/mockData.ts
+++ src/__mocks__/mocksForMissingEndpoints/mockData.ts
@@ -1,8 +1,6 @@
-import { RolleDto } from "../../api/BidragBehandlingApi";
 import { createAInntektData } from "../testdata/aInntektTestData";
 import { arbeidsforholdData } from "../testdata/arbeidsforholdTestData";
 import { createSkattegrunnlag } from "../testdata/grunnlagTestData";
-import { inntektData } from "../testdata/inntektTestData";
 
 export const initMockData = () => {
     if (!localStorage.getItem("arbeidsforhold")) {
@@ -14,12 +12,4 @@ export const initMockData = () => {
     if (!localStorage.getItem("ainntekt")) {
         localStorage.setItem(`ainntekt`, JSON.stringify(createAInntektData()));
     }
-};
-
-export const inntektMockData = (behandlingId: string, roller: RolleDto[]) => {
-    if (!localStorage.getItem(`inntekt-${behandlingId}`)) {
-        localStorage.setItem(`inntekt-${behandlingId}`, JSON.stringify(inntektData(roller)));
-    }
-
-    return JSON.parse(localStorage.getItem(`inntekt-${behandlingId}`));
 };
