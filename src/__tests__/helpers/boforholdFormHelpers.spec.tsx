9efb66a669bbaf75e969ad4d8db033d8af9e2b5e
markodraksic <draksic.m@gmail.com>
2023-10-09 16:01:27 +0200

    Fix check for overlapping periods in boforhold, add tests

--- src/__tests__/helpers/boforholdFormHelpers.spec.tsx
+++ src/__tests__/helpers/boforholdFormHelpers.spec.tsx
@@ -1,7 +1,7 @@
 import { expect } from "chai";
 import { describe } from "mocha";
 
-import { fillInPeriodGaps } from "../../components/forms/helpers/boforholdFormHelpers";
+import { checkOverlappingPeriods, fillInPeriodGaps } from "../../components/forms/helpers/boforholdFormHelpers";
 
 describe("BoforholdFormHelpers", () => {
     it("should fill in a period with status IKKE IKKE_REGISTRERT_PA_ADRESSE if there is a gap between 2 periods in folkeregistre", () => {
@@ -58,5 +58,48 @@ describe("BoforholdFormHelpers", () => {
             expect(husstandsOpplysning.tilDato?.toDateString()).equals(expectedResult[i].tilDato?.toDateString());
             expect(husstandsOpplysning.boStatus).equals(expectedResult[i].boStatus);
         });
+    });
+
+    it("should return an array with overlapping periods if there are overlaps", () => {
+        const testPeriods = [
+            { datoFom: "2022-01-01", datoTom: "2022-03-31" },
+            { datoFom: "2022-03-01", datoTom: "2022-04-30" },
+            { datoFom: "2022-04-01", datoTom: "2022-07-31" },
+        ];
+        const expectedResult = [
+            ["2022-03-31", "2022-03-01"],
+            ["2022-04-30", "2022-04-01"],
+        ];
+        const overlappingPeriods = checkOverlappingPeriods(testPeriods);
+        expect(overlappingPeriods.length).equals(expectedResult.length);
+        overlappingPeriods.forEach((periods, i) => {
+            expect(periods[0]).equals(expectedResult[i][0]);
+            expect(periods[1]).equals(expectedResult[i][1]);
+        });
+    });
+
+    it("should return an array with overlapping periods if one of the periods that is not last has null as datoTom", () => {
+        const testPeriods = [
+            { datoFom: "2022-01-01", datoTom: "2022-03-31" },
+            { datoFom: "2022-04-01", datoTom: null },
+            { datoFom: "2022-05-01", datoTom: "2022-07-01" },
+        ];
+        const expectedResult = [["null", "2022-05-01"]];
+        const overlappingPeriods = checkOverlappingPeriods(testPeriods);
+        expect(overlappingPeriods.length).equals(expectedResult.length);
+        overlappingPeriods.forEach((periods, i) => {
+            expect(periods[0]).equals(expectedResult[i][0]);
+            expect(periods[1]).equals(expectedResult[i][1]);
+        });
+    });
+
+    it("should return an empty array with overlapping periods if the last period that has null as datoTom", () => {
+        const testPeriods = [
+            { datoFom: "2022-01-01", datoTom: "2022-03-31" },
+            { datoFom: "2022-04-01", datoTom: "2022-04-30" },
+            { datoFom: "2022-05-01", datoTom: null },
+        ];
+        const overlappingPeriods = checkOverlappingPeriods(testPeriods);
+        expect(overlappingPeriods.length).equals(0);
     });
 });
