5a16c22df3efe0a27e0c02474cd655af1f8c265b
Ugur Cenar <cenar93@gmail.com>
2023-11-20 11:32:14 +0100

    Fix test

--- .mocharc.js
+++ .mocharc.js
@@ -1,6 +1,6 @@
 module.exports = {
   require: [
-    "src/__tests__/resources/env.init.js", '@swc/register', "esm", "global-jsdom/register", 'ts-node/register/transpile-only', "ignore-styles", "src/__tests__/resources/mocha.init.ts"
+    "src/__tests__/resources/env.init.js", 'esbuild-register', "esm", "global-jsdom/register", 'ts-node/register/transpile-only', "ignore-styles", "src/__tests__/resources/mocha.init.ts"
   ],
   extension: ["js", "ts", "tsx"],
   package: './package.json',
