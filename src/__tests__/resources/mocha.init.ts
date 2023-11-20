8a290c64a9b30d16ecb7ee3ed07f218f25c3cbe6
Ugur Cenar <cenar93@gmail.com>
2023-11-20 12:09:34 +0100

    Eslint fixes

--- src/__tests__/resources/mocha.init.ts
+++ src/__tests__/resources/mocha.init.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import "cross-fetch/polyfill";
 
 import { cleanup } from "@testing-library/react";
@@ -22,10 +23,10 @@ export async function mochaGlobalSetup() {
 
     // @ts-ignore
     global.window.logToServer = {
-        info: (message: string) => null,
-        warning: (message: string) => null,
-        debug: (message: string) => null,
-        error: (message: string, err: Error) => null,
+        info: (_message: string) => null,
+        warning: (_message: string) => null,
+        debug: (_message: string) => null,
+        error: (_message: string, _err: Error) => null,
     };
 
     beforeEach(() => {
