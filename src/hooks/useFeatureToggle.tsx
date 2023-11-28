df38374ddf351272a47023b39ff88f645b1ff5a0
Ugur Alpay Cenar <cenar93@gmail.com>
2023-11-28 09:40:56 +0100

    Fix feature toggle (#86)
    * Fix feature toggle
    
    * Debug logging
    
    * Remove retry
    
    * Refaktor
    
    * Upgrade dependencies

--- src/hooks/useFeatureToggle.tsx
+++ src/hooks/useFeatureToggle.tsx
@@ -1,27 +1,47 @@
 import { SecuritySessionUtils } from "@navikt/bidrag-ui-common";
 import { useSuspenseQuery } from "@tanstack/react-query";
+import { useEffect } from "react";
 
 export default function useFeatureToogle() {
+    const isMockEnabled = process.env.ENABLE_MOCK == "true";
     const { data: userId } = useSuspenseQuery({
         queryKey: ["user"],
         queryFn: () => SecuritySessionUtils.hentSaksbehandlerId(),
-        initialData: "",
+        initialData: () => (isMockEnabled ? "" : undefined),
+        staleTime: isMockEnabled ? 0 : Infinity,
     });
-    const enableVedtakSkjermbilde = process.env.ENABLE_VEDTAK_SKJERMBILDE;
-    const enableInntektSkjermbilde = process.env.ENABLE_INNTEKT_SKJERMBILDE;
+    const enableVedtakSkjermbilde = process.env.ENABLE_VEDTAK_SKJERMBILDE == "true";
+    const enableInntektSkjermbilde = process.env.ENABLE_INNTEKT_SKJERMBILDE == "true";
     const enableFatteVedtak = process.env.ENABLE_FATTE_VEDTAK;
-    console.debug(
-        `enableVedtakSkjermbilde=${enableVedtakSkjermbilde} enableInntektSkjermbilde=${enableInntektSkjermbilde} enableFatteVedtak=${enableFatteVedtak} process.env.ENABLE_INNTEKT_SKJERMBILDE=${process.env.ENABLE_INNTEKT_SKJERMBILDE} process.env.ENABLE_VEDTAK_SKJERMBILDE=${process.env.ENABLE_VEDTAK_SKJERMBILDE}`
-    );
+
+    function getUserIdsEnabledFor(value: string): string[] {
+        if (value == "false" || value == "true") {
+            return [];
+        }
+        return value?.split(",").map((v) => v.trim()) ?? [];
+    }
+    const userIdsInntekt = getUserIdsEnabledFor(process.env.ENABLE_INNTEKT_SKJERMBILDE);
+    const userIdsVedtak = getUserIdsEnabledFor(process.env.ENABLE_VEDTAK_SKJERMBILDE);
+
+    useEffect(() => {
+        console.debug(
+            `enableVedtakSkjermbilde=${enableVedtakSkjermbilde} enableInntektSkjermbilde=${enableInntektSkjermbilde} enableFatteVedtak=${enableFatteVedtak}`
+        );
+        console.debug(
+            "UserIds",
+            userId,
+            "vedtak",
+            userIdsVedtak,
+            userIdsVedtak.includes(userId),
+            "inntekt",
+            userIdsInntekt,
+            userIdsInntekt.includes(userId)
+        );
+    }, [userId]);
+
     return {
         isFatteVedtakEnabled: enableFatteVedtak,
-        isInntektSkjermbildeEnabled:
-            enableInntektSkjermbilde == undefined ||
-            enableInntektSkjermbilde == "true" ||
-            enableInntektSkjermbilde.includes(userId),
-        isVedtakSkjermbildeEnabled:
-            enableVedtakSkjermbilde == undefined ||
-            enableVedtakSkjermbilde == "true" ||
-            enableVedtakSkjermbilde.includes(userId),
+        isInntektSkjermbildeEnabled: enableInntektSkjermbilde || userIdsInntekt.includes(userId),
+        isVedtakSkjermbildeEnabled: enableVedtakSkjermbilde || userIdsVedtak.includes(userId),
     };
 }
