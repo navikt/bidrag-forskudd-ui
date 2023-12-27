0f3991d6136a1a14b8ce9b2d3089ce2c98af2140
Ugur Cenar <cenar93@gmail.com>
2023-12-27 12:44:23 +0100

    Fix

--- src/hooks/useFeatureToggle.tsx
+++ src/hooks/useFeatureToggle.tsx
@@ -1,6 +1,6 @@
 import { SecuritySessionUtils } from "@navikt/bidrag-ui-common";
 import { useSuspenseQuery } from "@tanstack/react-query";
-import { useFlag, useUnleashContext } from "@unleash/proxy-client-react";
+import { useFlag, useUnleashClient } from "@unleash/proxy-client-react";
 import { useEffect } from "react";
 
 export default function useFeatureToogle() {
@@ -8,22 +8,23 @@ export default function useFeatureToogle() {
     const enableInntektSkjermbilde = useFlag("behandling.skjermbilde.inntekter");
     const enableVedtakSkjermbilde = useFlag("behandling.skjermbilde.vedtak");
     const enableFatteVedtak = useFlag("behandling.fattevedtak");
-    const updateContext = useUnleashContext();
+    const client = useUnleashClient();
     const { data: userId } = useSuspenseQuery({
         queryKey: ["user"],
-        queryFn: () => SecuritySessionUtils.hentSaksbehandlerId(),
+        queryFn: async () => SecuritySessionUtils.hentSaksbehandlerId(),
         initialData: () => (isMockEnabled ? "" : undefined),
         staleTime: isMockEnabled ? 0 : Infinity,
     });
 
     useEffect(() => {
-        updateContext({
+        client.updateContext({
             userId,
             properties: {
                 inforingsgruppen: userId,
                 testbrukere: userId,
             },
         });
+        console.log(client.getAllToggles(), client.getContext());
     }, [userId]);
 
     useEffect(() => {
