c5cf3caed2ec18eb1d5e2f9dbcafb73f7abfba53
markodraksic <draksic.m@gmail.com>
2024-08-02 12:36:18 +0200

    Have text adjusted to type of behandling in the header

--- src/common/components/header/BidragBehandlingHeader.tsx
+++ src/common/components/header/BidragBehandlingHeader.tsx
@@ -1,3 +1,4 @@
+import { TypeBehandling } from "@api/BidragBehandlingApiV1";
 import { useBehandlingProvider } from "@common/context/BehandlingContext";
 import { SakHeader } from "@navikt/bidrag-ui-common";
 import React, { memo } from "react";
@@ -5,9 +6,14 @@ import React, { memo } from "react";
 import text from "../../constants/texts";
 import { useGetBehandlingV2, usePersonsQueries } from "../../hooks/useApiData";
 
+const behandlingTypeTextMapper = {
+    [TypeBehandling.FORSKUDD]: text.skjermbildeNavn.forskudd,
+    [TypeBehandling.SAeRBIDRAG]: text.skjermbildeNavn.særbidrag,
+};
+
 export const Header = memo(() => {
     const { behandlingId, vedtakId } = useBehandlingProvider();
-    const { roller, saksnummer } = useGetBehandlingV2();
+    const { roller, saksnummer, type } = useGetBehandlingV2();
     const personsQueries = usePersonsQueries(roller);
     const rollerMedPersonNavn = personsQueries.map(({ data }) => data);
     return (
@@ -18,7 +24,7 @@ export const Header = memo(() => {
                 ident: person.ident!,
                 navn: person.visningsnavn,
             }))}
-            skjermbilde={{ navn: text.skjermbildeNavn, referanse: `#${behandlingId ?? vedtakId}` }}
+            skjermbilde={{ navn: behandlingTypeTextMapper[type], referanse: `#${behandlingId ?? vedtakId}` }}
         />
     );
 });
