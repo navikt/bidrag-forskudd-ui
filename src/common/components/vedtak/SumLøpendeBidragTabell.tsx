000cb4dac71d6afd06ffc2a52ca741c9a57f3b3f
Ugur Alpay Cenar <cenar93@gmail.com>
2024-10-10 09:37:03 +0200

    Fiks sum løpende bidrag

--- src/common/components/vedtak/SumLøpendeBidragTabell.tsx
+++ src/common/components/vedtak/SumLøpendeBidragTabell.tsx
@@ -7,19 +7,20 @@ export const SumLøpendeBidragTabell = () => {
     const { data: beregnetSærbidrag } = useGetBeregningSærbidrag();
 
     const delberegning = beregnetSærbidrag.resultat.delberegningSumLøpendeBidrag;
-    const colPaddingClassname = "px-4";
+    const colPaddingClassname = "px-1";
     return (
         <div>
             <Heading size="xsmall">{"BPs løpende bidrag"}</Heading>
             <table className="table-auto pb-[5px] border-collapse text-left border-spacing-2 w-full">
                 <thead>
                     <tr>
-                        <th className="pr-4">{"Barn"}</th>
+                        <th className="pr-1">{"Barn"}</th>
                         <th className={`${colPaddingClassname}`}>{"Saksnummer"}</th>
-                        <th className={`${colPaddingClassname}`}>{"Løpende bidrag"}</th>
-                        <th className={`${colPaddingClassname}`}>{"Samværsfradrag"}</th>
-                        <th className={`${colPaddingClassname}`}>{"Beregnet bidrag"}</th>
-                        <th className={`${colPaddingClassname}`}>{"Resultat"}</th>
+                        <th className={`${colPaddingClassname} text-right`}>{"Løpende bidrag"}</th>
+                        <th className={`${colPaddingClassname} text-right`}>{"Samværsfradrag"}</th>
+                        <th className={`${colPaddingClassname} text-right`}>{"Faktisk bidrag"}</th>
+                        <th className={`${colPaddingClassname} text-right`}>{"Beregnet bidrag"}</th>
+                        <th className={`${colPaddingClassname} text-right`}>{"Resultat"}</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -34,6 +35,9 @@ export const SumLøpendeBidragTabell = () => {
                                 {formatterBeløpForBeregning(row.samværsfradrag, true)}
                             </td>
                             <td className={`${colPaddingClassname} text-right`}>
+                                {formatterBeløpForBeregning(row.faktiskBeløp, true)}
+                            </td>
+                            <td className={`${colPaddingClassname} text-right`}>
                                 {formatterBeløpForBeregning(row.beregnetBeløp, true)}
                             </td>
                             <td className={`${colPaddingClassname} text-right`}>
@@ -42,8 +46,8 @@ export const SumLøpendeBidragTabell = () => {
                         </tr>
                     ))}
                     <tr className="border-t border-solid border-black border-b-2 border-l-0 border-r-0">
-                        <td colSpan={4} className="text-right"></td>
-                        <td colSpan={1} className=" ">
+                        {/* <td colSpan={5} className="text-right"></td> */}
+                        <td colSpan={6} className="text-right ">
                             {"Sum løpende bidrag:"}
                         </td>
                         <td colSpan={1} className={`${colPaddingClassname} text-right`}>
