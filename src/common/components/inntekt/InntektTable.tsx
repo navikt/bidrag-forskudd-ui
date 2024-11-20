ee8d7abb0c82a7d0d47268571e6ce7e8888241e1
markodraksic <draksic.m@gmail.com>
2024-11-20 15:54:46 +0100

    Add overlay loader to inntekt tables

--- src/common/components/inntekt/InntektTable.tsx
+++ src/common/components/inntekt/InntektTable.tsx
@@ -10,6 +10,7 @@ import { BehandlingAlert } from "@common/components/BehandlingAlert";
 import { FormControlledCheckbox } from "@common/components/formFields/FormControlledCheckbox";
 import { FormControlledMonthPicker } from "@common/components/formFields/FormControlledMonthPicker";
 import { FormControlledTextField } from "@common/components/formFields/FormControlledTextField";
+import { OverlayLoader } from "@common/components/OverlayLoader";
 import text from "@common/constants/texts";
 import { useBehandlingProvider } from "@common/context/BehandlingContext";
 import {
@@ -469,13 +470,18 @@ export const InntektTabel = ({
                     )}
                 </BehandlingAlert>
             )}
-            {children({
-                controlledFields,
-                onEditRow,
-                onSaveRow,
-                addPeriod,
-                handleOnSelect,
-            })}
+            <div
+                className={`${saveInntekt.mutation.isPending ? "relative" : "inherit"} block overflow-x-auto whitespace-nowrap`}
+            >
+                <OverlayLoader loading={saveInntekt.mutation.isPending} />
+                {children({
+                    controlledFields,
+                    onEditRow,
+                    onSaveRow,
+                    addPeriod,
+                    handleOnSelect,
+                })}
+            </div>
         </>
     );
 };
