eaec20bf45840ea258ef85ffc14511e087cbcced
markodraksic <draksic.m@gmail.com>
2024-12-03 15:48:19 +0100

    Use textarea for comment fields in tables, fix css and error handling

--- src/common/helpers/keyboardHelpers.ts
+++ src/common/helpers/keyboardHelpers.ts
@@ -1,5 +1,5 @@
 export const actionOnEnter = (event: () => void) => (e: React.KeyboardEvent) => {
-    if (e.key === "Enter") {
+    if (e.key === "Enter" && !(e.target instanceof HTMLElement && e.target.nodeName === "TEXTAREA")) {
         e.preventDefault();
         event();
     }
